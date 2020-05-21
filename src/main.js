26bce1bedc6d408bf607a135bf61e127ad8fc57d
辉 曾 <zenghui@shanghai3h.com>
2020-05-21 17:29:38 +0800

    202005

--- src/main.js
+++ src/main.js
@@ -8,7 +8,7 @@ import 'element-ui/lib/theme-chalk/index.css';
 import './style/index.css'
 import './style/element-ui.css'
 Vue.use(ElementUI);
-
+  
 Vue.config.productionTip = false
 new Vue({
   router,
